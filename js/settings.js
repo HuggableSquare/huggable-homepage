750a6c2885aa72664ddba54142ac4c1e3d5e280b
HuggableSquare <huggablesquare@gmail.com>
2016-10-03 22:47:29 -0400

    settings cleanup

--- js/settings.js
+++ js/settings.js
@@ -5,63 +5,70 @@ if (localStorage.getItem('linkSetting') === null) localStorage.setItem("linkSett
 if (localStorage.getItem('hourSetting') === null) localStorage.setItem("hourSetting", true);
 if (localStorage.getItem('loveSetting') === null) localStorage.setItem("loveSetting", true);
 
-$(document).ready(function () {  
+$(document).ready(function () {
   // Using jQuery's toggle function to display items based on localStorage
   // JSON.parse is necessary because browsers don't support booleans
   // in localStorage (for some reason >.>)
-  
+
+  var $clock = $('#clock');
+  var $date = $('#date');
+  var $links = $('#links');
+  var $meridiem = $('#meridiem');
+  var $hours12 = $('#hours12');
+  var $hours24 = $('#hours24');
+  var $made1 = $('#made1');
+  var $made2 = $('#made2');
+
   // clockSetting
-  $("#clock").toggle(JSON.parse(localStorage.clockSetting));
-  
+  $clock.toggle(JSON.parse(localStorage.clockSetting));
+
   // dateSetting
-  $("#Date").toggle(JSON.parse(localStorage.dateSetting));
-  $("#datespacefix").toggle(JSON.parse(localStorage.dateSetting));
-  
+  $date.toggle(JSON.parse(localStorage.dateSetting));
+
   // linkSetting
-  $("#links").toggle(JSON.parse(localStorage.linkSetting));
-  
+  $links.toggle(JSON.parse(localStorage.linkSetting));
+
   // hourSetting
-  $("#meridiem").toggle(JSON.parse(localStorage.hourSetting));
-  $("#hours12").toggle(JSON.parse(localStorage.hourSetting));
+  $meridiem.toggle(JSON.parse(localStorage.hourSetting));
+  $hours12.toggle(JSON.parse(localStorage.hourSetting));
   // fixed?
-  $("#hours24").toggle(!JSON.parse(localStorage.hourSetting));
-  
+  $hours24.toggle(!JSON.parse(localStorage.hourSetting));
+
   // loveSetting
-  $("#made1").toggle(JSON.parse(localStorage.loveSetting));
-  $("#made2").toggle(JSON.parse(localStorage.loveSetting));
-  
+  $made1.toggle(JSON.parse(localStorage.loveSetting));
+  $made2.toggle(JSON.parse(localStorage.loveSetting));
+
   // Click functions
-  $("#clockSetting").click(function () {
+  $('#clockSetting').click(function () {
     localStorage.clockSetting = !JSON.parse(localStorage.clockSetting);
-    
-    $("#clock").toggle(JSON.parse(localStorage.clockSetting));
+
+    $clock.toggle(JSON.parse(localStorage.clockSetting));
   });
 
-  $("#dateSetting").click(function () {
+  $('#dateSetting').click(function () {
     localStorage.dateSetting = !JSON.parse(localStorage.dateSetting);
-    
-    $("#Date").toggle(JSON.parse(localStorage.dateSetting));
-    $("#datespacefix").toggle(JSON.parse(localStorage.dateSetting));
+
+    $date.toggle(JSON.parse(localStorage.dateSetting));
   });
-  
-  $("#linkSetting").click(function () {
+
+  $('#linkSetting').click(function () {
     localStorage.linkSetting = !JSON.parse(localStorage.linkSetting);
-    
-    $("#links").toggle(JSON.parse(localStorage.linkSetting));
+
+    $links.toggle(JSON.parse(localStorage.linkSetting));
   });
-  
-  $("#clock").click(function () {
+
+  $('#clock').click(function () {
     localStorage.hourSetting = !JSON.parse(localStorage.hourSetting);
-    
-    $("#meridiem").toggle(JSON.parse(localStorage.hourSetting));
-    $("#hours12").toggle(JSON.parse(localStorage.hourSetting));
-    $("#hours24").toggle(!JSON.parse(localStorage.hourSetting));
+
+    $meridiem.toggle(JSON.parse(localStorage.hourSetting));
+    $hours12.toggle(JSON.parse(localStorage.hourSetting));
+    $hours24.toggle(!JSON.parse(localStorage.hourSetting));
   });
-  
-  $("#loveSetting").click(function () {
+
+  $('#loveSetting').click(function () {
     localStorage.loveSetting = !JSON.parse(localStorage.loveSetting);
-    
-    $("#made1").toggle(JSON.parse(localStorage.loveSetting));
-    $("#made2").toggle(JSON.parse(localStorage.loveSetting));
+
+    $made1.toggle(JSON.parse(localStorage.loveSetting));
+    $made2.toggle(JSON.parse(localStorage.loveSetting));
   });
 });
